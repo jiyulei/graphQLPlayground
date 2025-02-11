b695fd7f1c7ea6c6fa23a1bbb48ab6ea659f960b
JiyuLei <jerrylei.ui@gmail.com>
2025-02-10 16:46:21 -0800

    Graphql | editUser (axios patch)

--- schema/schema.js
+++ schema/schema.js
@@ -46,26 +46,41 @@ const mutation = new GraphQLObjectType({
     addUser: {
       type: UserType,
       args: {
-        firstName: { type: new GraphQLNonNull(GraphQLString)},
-        age: { type: new GraphQLNonNull(GraphQLString)},
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
         companyId: { type: GraphQLString },
       },
-      resolve(parentValue, { firstName, age}) {
+      resolve(parentValue, { firstName, age }) {
         return axios
           .post(`http://localhost:3000/users`, { firstName, age })
           .then((res) => res.data);
-      }
+      },
     },
     deleteUser: {
-        type: UserType,
-        args: {
-            userId: { type: new GraphQLNonNull(GraphQLString)},
-        },
-        resolve(parentValue, { userId }) {
-            return axios.delete(`http://localhost:3000/users/${userId}`)
-            .then(res => res.data);
-        }
-    }
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parentValue, { id }) {
+        return axios
+          .delete(`http://localhost:3000/users/${id}`)
+          .then((res) => res.data);
+      },
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString },
+      },
+      resolve(parentValue, args) {
+        return axios
+          .patch(`http://localhost:3000/users/${args.id}`, args)
+          .then((res) => res.data);
+      },
+    },
   },
 });
 
